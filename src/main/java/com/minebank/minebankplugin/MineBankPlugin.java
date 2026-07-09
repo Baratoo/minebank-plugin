3e3bede38a94143382de2b7201fd5d1706c3e307
Baratoo <kaique_luiz1@hotmail.com>
2026-07-08 22:01:36 -0300

    Criado Plugin para integração minecraft com MinebankAPI

--- src/main/java/com/minebank/minebankplugin/MineBankPlugin.java
+++ src/main/java/com/minebank/minebankplugin/MineBankPlugin.java
@@ -1,4 +1,17 @@
 package com.minebank.minebankplugin;
 
-public class MineBankPlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineBankPlugin extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        //Roda sozinho assim que o servidor inicia
+        getLogger().info("Plugin iniciado...");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin desligando...");
+    }
 }
